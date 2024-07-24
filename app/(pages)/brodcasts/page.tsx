50325264a431165ea97cb72ae92df6fad088f33d
kasim <kasimkazmi27@gmail.com>
2024-07-23 21:06:15 -0400

    minor fixes

---
+++ app/(pages)/brodcasts/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="flex justify-center py-24 bg-S2BG items-center">
+      Broadcast Page
+    </section>
+  );
+};
+
+export default page;
