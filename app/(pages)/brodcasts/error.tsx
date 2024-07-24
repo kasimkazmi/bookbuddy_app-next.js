773007debd95781520c4b3b0da831ad32e051dc0
kasim <kasimkazmi27@gmail.com>
2024-07-23 20:51:23 -0400

    Broadcast error file added

---
+++ app/(pages)/brodcasts/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const error = () => {
+  return (
+    <div className="flex justify-center items-center">
+      Broadcast screen error
+    </div>
+  );
+};
+
+export default error;
