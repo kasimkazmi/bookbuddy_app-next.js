7cdcb5bbf89b8648b9b7018cc0eb995b9c6a0a8b
Kasim Kazmi <kasimkazmi27@gmail.com>
2025-06-23 21:36:44 -0400

    Refactor layout and header components for improved structure; remove unused CSS file

--- src/app/(layout-pages)/layout.tsx
+++ src/app/(layout-pages)/layout.tsx
@@ -1,24 +1,16 @@
-import Header from "@/src/views/landingpage/headersection"
-import Footer from "@/src/views/landingpage/footersection"
+import Header from '@/src/views/landingpage/headersection';
+import Footer from '@/src/views/landingpage/footersection';
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <>
-    <div style={{
-          // position: "fixed",
-          // top: 0,
-          // width: "100%",
-          // zIndex: 500,
-        }}>
-      <Header
-      />
-        </div>
-      <div >{children}</div> {/* Adjust paddingTop as needed */}
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <div>{children}</div>
+            <Footer />
+        </>
+    );
 }
