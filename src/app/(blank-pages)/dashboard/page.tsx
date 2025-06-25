1ef83ef56ca60ef952449186f7314e7dadebdffc
Kasim Kazmi <kasimkazmi27@gmail.com>
2025-06-24 21:59:05 -0400

    feat: create initial dashboard page layout with construction notice

---
+++ src/app/(blank-pages)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function page() {
+    return (
+        <section className="flex justify-center py-24 bg-S4BG items-center">
+            <div className="bg-S1BG rounded-3xl py-16 px-4 sm:px-6 w-full max-w-6xl">
+                <h1 className="text-3xl font-bold text-center mb-8 text-S1Text">
+                    Dashboard is under construction
+                </h1>
+            </div>
+        </section>
+    );
+}
